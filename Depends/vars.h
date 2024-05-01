19caa5274ac65201f38872434b12d0cf5994b853
Michael <mchapman3@zagmail.gonzaga.edu>
2024-05-01 15:28:15 -0700

    Added basic ESTOP

--- Depends/vars.h
+++ Depends/vars.h
@@ -2,4 +2,16 @@
 #define VARS_H
 
 int driverReady = 0;
+
+// Motor Groups
+int motorFrontLeft;
+int motorCenterLeft;
+int motorBackLeft;
+int motorFrontRight;
+int motorCenterRight;
+int motorBackRight;
+// Motor Arrays
+
+int leftMotorGroup[3];
+int rightMotorGroup[3];
 #endif
--- SerialCommandProcessor.ino
+++ SerialCommandProcessor.ino
@@ -1,6 +1,18 @@
 #include <Arduino.h>
+#include "Depends/vars.h"
+
 void decodeCommand()
 {
     String incomingSerial = Serial.readString();
+    if (incomingSerial == "ESTOP")
+    {
+        // Set the driver ready flag to 0
+        driverReady = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            analogWrite(leftMotorGroup[i], 0);
+            analogWrite(rightMotorGroup[i], 0);
+        }
+    }
     // TODO: Add command decoding
 }
