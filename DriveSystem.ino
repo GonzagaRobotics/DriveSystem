3fa917a762252c6cfbaa39f3e6f5f1520577af5e
Michael <mchapman3@zagmail.gonzaga.edu>
2024-05-01 11:29:35 -0700

    Added Main files
    Started work on reimplementing the motor stuffs

---
+++ DriveSystem.ino
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "motorControl.cpp"
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    void motorDriver();
+}
---
+++ motorControl.cpp
@@ -0,0 +1,4 @@
+void motorDriver()
+{
+    Serial.print("TODO: Motors");
+}
