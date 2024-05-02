d8bbbe1bb3ea6e87472062e20bf689f78e8bb2e5
Michael <mchapman3@zagmail.gonzaga.edu>
2024-05-02 16:52:12 -0700

    commit message here

--- SerialBridge.ino
+++ SerialBridge.ino
@@ -4,6 +4,23 @@
 void decodeCommand()
 {
     Serial.readBytes(inputStream, MAX_BYTES);
-
+    for (int i = 0; i < MAX_BYTES; i++)
+    {
+        if (inputStream[i] == 'E')
+        {
+        }
+        if (inputStream[i] == 'F')
+        {
+        }
+        if (inputStream[i] == 'B')
+        {
+        }
+        if (inputStream[i] == 'L')
+        {
+        }
+        if (inputStream[i] == 'R')
+        {
+        }
+    }
     // TODO: Add command decoding
 }
