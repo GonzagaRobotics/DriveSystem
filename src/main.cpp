7599b5e2d12f68a35ac1114b68c04127a0a8d5bd
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2026-01-03 20:58:31 -0800

    Add basic cmd reading and pwm output

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,68 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int numMotors = 6;
+// TODO: Actual pin assignments
+const int motorFL = 0;
+const int motorFR = 0;
+const int motorCL = 0;
+const int motorCR = 0;
+const int motorRL = 0;
+const int motorRR = 0;
+const int motorPins[numMotors] = {motorFL, motorFR, motorCL, motorCR, motorRL, motorRR};
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+JsonDocument doc;
+unsigned long lastCmdTime = 0;
+int motorCmds[numMotors] = {0, 0, 0, 0, 0, 0};
+
+void setup()
+{
+  Serial.begin(115200);
+
+  for (int i = 0; i < numMotors; i++)
+  {
+    pinMode(motorPins[i], OUTPUT);
+  }
+
+  while (!Serial)
+  {
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  // Read incoming serial data
+  while (Serial.available() > 0)
+  {
+    DeserializationError error = deserializeJson(doc, Serial);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    lastCmdTime = millis();
+    for (int i = 0; i < numMotors; i++)
+    {
+      motorCmds[i] = doc["motors"][i];
+    }
+  }
+
+  // As an extra safety measure, stop all motors if no command is received for 500 ms
+  if (millis() - lastCmdTime > 500)
+  {
+    for (int i = 0; i < numMotors; i++)
+    {
+      motorCmds[i] = 0;
+    }
+  }
+
+  for (int i = 0; i < numMotors; i++)
+  {
+    analogWrite(motorPins[i], motorCmds[i]);
+  }
+
+  delay(20);
 }
